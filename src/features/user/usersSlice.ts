2f93a54bf8b414b1e732905548c5b69580dc1928
Christian Castaneda <christianjc_09@yahoo.com>
2023-03-22 16:13:26 -0700

    changed initial state for navbuttons

--- src/features/user/usersSlice.ts
+++ src/features/user/usersSlice.ts
@@ -5,7 +5,7 @@ const usersSlice = createSlice({
     name: 'users',
     initialState: {
         memberNavState: { id: 'Dashboard', tab: 0, path: '/account/member', active: 0 },
-        managerNavState: { id: 'Dashboard', tab: 0, path: '/account/manager', active: 0 },
+        managerNavState: { id: 'Schedule', tab: 0, path: '/account/manager', active: 0 },
         supervisorNavState: {
             id: 'Dashboard',
             tab: 0,
