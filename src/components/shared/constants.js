da4cc11fd9324cdb0528d5a049cb2552f4dee0d2
Christian Castaneda <christianjc_09@yahoo.com>
2023-03-22 19:41:51 -0700

    Updated buttons and tab content

--- src/components/shared/constants.js
+++ src/components/shared/constants.js
@@ -1,112 +1,112 @@
-import DashboardCustomizeRoundedIcon from "@mui/icons-material/DashboardCustomizeRounded";
-import CalendarMonthRoundedIcon from "@mui/icons-material/CalendarMonthRounded";
-import StorefrontRoundedIcon from "@mui/icons-material/StorefrontRounded";
-import SettingsIcon from "@mui/icons-material/Settings";
+import DashboardCustomizeRoundedIcon from '@mui/icons-material/DashboardCustomizeRounded'
+import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded'
+import StorefrontRoundedIcon from '@mui/icons-material/StorefrontRounded'
+import SettingsIcon from '@mui/icons-material/Settings'
 
 export const memberTabs = {
-  Schedule: [{ id: "Individual" }, { id: "All Shifts" }],
-  Members: [{ id: "Information" }],
-  Settings: [{ id: "Overview" }, { id: "Tab2" }],
-};
+    Schedule: [{ id: 'Individual' }, { id: 'All Shifts' }],
+    Members: [{ id: 'Information' }],
+    Settings: [{ id: 'Information' }, { id: 'Availability' }, { id: 'Preferences' }],
+}
 
 export const managerTabs = {
-  Schedule: [{ id: "All Shifts" }, { id: "Individual" }],
-  Planner: [{ id: "Unassigned" }, { id: "Assigned" }, { id: "Categories" }],
-  Members: [{ id: "Information" }, { id: "Availability" }],
-};
+    Schedule: [{ id: 'All Shifts' }, { id: 'Individual' }],
+    Planner: [{ id: 'Unassigned' }, { id: 'Assigned' }, { id: 'Categories' }],
+    Members: [{ id: 'Information' }, { id: 'Availability' }],
+}
 export const supervisorTabs = {
-  Schedule: [{ id: "All Shifts" }, { id: "Individual" }],
-  Planner: [{ id: "Unassigned" }, { id: "Assigned" }],
-  Members: [{ id: "Information" }, { id: "Availability" }],
-};
+    Schedule: [{ id: 'All Shifts' }, { id: 'Individual' }],
+    Planner: [{ id: 'Unassigned' }, { id: 'Assigned' }],
+    Members: [{ id: 'Information' }, { id: 'Availability' }],
+}
 
 //** Navbar Buttons for the members */
 export const memberCategories = [
-  {
-    id: "Member",
-    children: [
-      {
-        id: "Schedule",
-        path: "/account/member",
-        active: 1,
-        icon: <CalendarMonthRoundedIcon />,
-      },
-      {
-        id: "Members",
-        path: "/account/member",
-        active: 2,
-        icon: <StorefrontRoundedIcon />,
-      },
-    ],
-  },
-  {
-    id: "Other",
-    children: [
-      {
-        id: "Settings",
-        path: "/account/member",
-        active: 3,
-      },
-    ],
-  },
-];
+    {
+        id: 'Member',
+        children: [
+            {
+                id: 'Schedule',
+                path: '/account/member',
+                active: 1,
+                icon: <CalendarMonthRoundedIcon />,
+            },
+            {
+                id: 'Members',
+                path: '/account/member',
+                active: 2,
+                icon: <StorefrontRoundedIcon />,
+            },
+        ],
+    },
+    {
+        id: 'Other',
+        children: [
+            {
+                id: 'Settings',
+                path: '/account/member',
+                active: 3,
+            },
+        ],
+    },
+]
 
 export const managerCategories = [
-  {
-    id: "Manager",
-    children: [
-      {
-        id: "Schedule",
-        path: "/account/manager",
-        active: 0,
-        icon: <DashboardCustomizeRoundedIcon />,
-      },
-      {
-        id: "Planner",
-        path: "/account/manager",
-        active: 1,
-        icon: <CalendarMonthRoundedIcon />,
-      },
-      {
-        id: "Members",
-        path: "/account/manager",
-        active: 2,
-      },
-    ],
-  },
-];
+    {
+        id: 'Manager',
+        children: [
+            {
+                id: 'Schedule',
+                path: '/account/manager',
+                active: 0,
+                icon: <DashboardCustomizeRoundedIcon />,
+            },
+            {
+                id: 'Planner',
+                path: '/account/manager',
+                active: 1,
+                icon: <CalendarMonthRoundedIcon />,
+            },
+            {
+                id: 'Members',
+                path: '/account/manager',
+                active: 2,
+            },
+        ],
+    },
+]
 
 export const supervisorCategories = [
-  {
-    id: "Supervisor",
-    children: [
-      {
-        id: "Schedule",
-        path: "/account/supervisor",
-        active: 0,
-        icon: <DashboardCustomizeRoundedIcon />,
-      },
-      {
-        id: "Planner",
-        path: "/account/supervisor",
-        active: 1,
-        icon: <CalendarMonthRoundedIcon />,
-      },
-      {
-        id: "Members",
-        path: "/account/supervisor",
-        active: 2,
-      },
-    ],
-  },
-];
+    {
+        id: 'Supervisor',
+        children: [
+            {
+                id: 'Schedule',
+                path: '/account/supervisor',
+                active: 0,
+                icon: <DashboardCustomizeRoundedIcon />,
+            },
+            {
+                id: 'Planner',
+                path: '/account/supervisor',
+                active: 1,
+                icon: <CalendarMonthRoundedIcon />,
+            },
+            {
+                id: 'Members',
+                path: '/account/supervisor',
+                active: 2,
+            },
+        ],
+    },
+]
 
 const icons = {
-  Dashboard: <DashboardCustomizeRoundedIcon />,
-  Schedule: <CalendarMonthRoundedIcon />,
-  Planner: <CalendarMonthRoundedIcon />,
-  Profile: <SettingsIcon />,
-  Marketplace: <StorefrontRoundedIcon />,
-};
+    Dashboard: <DashboardCustomizeRoundedIcon />,
+    Schedule: <CalendarMonthRoundedIcon />,
+    Planner: <CalendarMonthRoundedIcon />,
+    Profile: <SettingsIcon />,
+    Marketplace: <StorefrontRoundedIcon />,
+}
 
-export const ROLES = ["Manager", "Member", "Supervisor"];
+export const ROLES = ['Manager', 'Member', 'Supervisor']
