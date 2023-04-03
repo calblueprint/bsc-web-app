de5983935fed0d20f36a1cc97b1beaf1df4da6db
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2023-04-03 12:55:06 -0700

    clean up from merge

--- src/components/manager/planner/unassignedTab/ManagerUnassignedTab.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UserTesting from "@/pages/testing/UserTesting";
-import { Typography } from "@mui/material";
-
-const ManagerUnassignedTab = () => {
-  return <h1>ManagerUnassignedTab</h1>;
-};
-
-export default ManagerUnassignedTab;
--- src/components/member/settings/availabilityTab/MemberAvailabilityTab.tsx
+++
@@ -1,8 +0,0 @@
-import AvailabilityTable from "@/features/user/tabels/AvailabilityTable";
-import React from "react";
-
-const MemberAvailabilityTab = () => {
-  return <h1>MemberAvailabilityTab</h1>;
-};
-
-export default MemberAvailabilityTab;
