d89311a1f18d5ffcdd7463f58c6b6490d1ba783a
Christian Castaneda <christianjc_09@yahoo.com>
2023-04-12 00:44:56 -0700

    added button functionality

--- src/features/userPreferences/items/PreferencesItem.tsx
+++ src/features/userPreferences/items/PreferencesItem.tsx
@@ -15,13 +15,19 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
 import Button from '@mui/material/Button'
 import Divider from '@mui/material/Divider'
-import { Dictionary } from '@reduxjs/toolkit'
-import { Shift } from '@/types/schema'
+import { Dictionary, EntityId } from '@reduxjs/toolkit'
+import { House, Shift } from '@/types/schema'
 import ToggleButton from '@mui/material/ToggleButton'
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 import { useSelector } from 'react-redux'
-import { selectCurrentUser } from '@/features/auth/authSlice'
+import {
+  selectCurrentHouse,
+  selectCurrentUser,
+} from '@/features/auth/authSlice'
 import { useEffect } from 'react'
+import { useUpdateShiftMutation } from '@/features/shift/shiftApiSlice'
+import { Snackbar, Alert } from '@mui/material'
+import { validatePreferences } from '@/utils/utils'
 
 function createData(
   name: string,
@@ -59,19 +65,70 @@ export default function PrefrencesItem(props: {
   shiftEntities: Dictionary<Shift>
 }) {
   const authUser = useSelector(selectCurrentUser)
+  const authHouse = useSelector(selectCurrentHouse) as House
   const { shiftsIds, category, shiftEntities } = props
   const [open, setOpen] = React.useState(false)
   const [authUserId, setAuthUserId] = React.useState('')
-
   const [alignment, setAlignment] = React.useState<string | null>(null)
+  const [isEditing, setIsEditing] = React.useState(true)
+  const [mainPreference, setMainPreference] = React.useState<
+    'No Preference' | 'Mix Preference' | 'Prefere All' | 'Dislike All'
+  >('No Preference')
+
+  const [shiftPreferences, setShiftPreference] = React.useState<{
+    [key: string]: {
+      newPreference: string | null
+      savedPreference: string | null
+      hasChanged: boolean
+    }
+  }>()
 
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
-    newAlignment: string,
-    id: string
+    newAlignment: string
   ) => {
     console.log(newAlignment)
+    const shiftPreferencesCopy = JSON.parse(JSON.stringify(shiftPreferences))
+
+    console.log(shiftPreferencesCopy)
+    if (shiftPreferences) {
+      shiftsIds.map((shiftId) => {
+        if (shiftPreferencesCopy[shiftId].savedPreference === newAlignment) {
+          shiftPreferencesCopy[shiftId].hasChanged = false
+        } else {
+          shiftPreferencesCopy[shiftId].hasChanged = true
+        }
+        shiftPreferencesCopy[shiftId].newPreference = newAlignment
+      })
+    }
+    console.log(shiftPreferencesCopy)
+
+    setShiftPreference(shiftPreferencesCopy)
+
     setAlignment(newAlignment)
+  }
+
+  const handleSingleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+    id: string
+  ) => {}
+
+  //** if true it opens the Succes message window */
+  const [openSuccessMsg, setOpenSuccessMsg] = React.useState(false)
+  //** if true it opens the Error message window */
+  const [openErrorMsg, setOpenErrorMsg] = React.useState(false)
+
+  //** Handles closing both success and error windows. */
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    setOpenSuccessMsg(false)
+    setOpenErrorMsg(false)
   }
 
   useEffect(() => {
@@ -80,7 +137,53 @@ export default function PrefrencesItem(props: {
     }
   }, [authUser])
 
-  return (
+  useEffect(() => {
+    if (shiftPreferences) {
+      const arr = Object.values(shiftPreferences).map(
+        (obj) => obj.newPreference
+      )
+      if (arr.every((value) => value === null)) {
+        setMainPreference('No Preference')
+        setAlignment(null)
+      } else if (arr.every((value) => value === 'prefere')) {
+        setMainPreference('Prefere All')
+        setAlignment('prefere')
+      } else if (arr.every((value) => value === 'dislike')) {
+        setMainPreference('Dislike All')
+        setAlignment('dislike')
+      } else {
+        setMainPreference('Mix Preference')
+        setAlignment(null)
+      }
+      console.log('updated Main Preference')
+    }
+  }, [shiftPreferences])
+
+  useEffect(() => {
+    if (shiftsIds) {
+      let obj = {}
+      const getPreference = shiftsIds.map((shiftId) => {
+        const preferences = validatePreferences(
+          shiftEntities[shiftId]?.preferences as Shift['preferences']
+        )
+        const isDislike = preferences.dislikedBy.includes(authUserId)
+        const isPrefere = preferences.preferredBy.includes(authUserId)
+        const choise = isPrefere ? 'prefere' : isDislike ? 'dislike' : null
+        obj = {
+          ...obj,
+          [shiftId]: {
+            savedPreference: choise,
+            newPreference: choise,
+            hasChanged: false,
+          },
+        }
+      })
+
+      setShiftPreference(obj)
+    }
+  }, [shiftsIds, authUserId, shiftEntities])
+
+  const content = (
     <React.Fragment>
       <Box display={'flex'}>
         <Box display={'flex'} paddingTop={2} paddingBottom={1}>
@@ -91,28 +194,79 @@ export default function PrefrencesItem(props: {
           >
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
-          <Typography variant="h6"> {category}</Typography>
+          <Typography variant="h6" sx={{ textTransform: 'capitalize' }}>
+            {category}
+          </Typography>
         </Box>
         <Box sx={{ flexGrow: 1 }} />
-        <Box display={'flex'} paddingTop={1} paddingBottom={1} marginRight={2}>
-          <ToggleButtonGroup
-            color="primary"
-            value={alignment}
-            exclusive
-            onChange={(event, value) => handleChange(event, value, 'all')}
-            aria-label="Platform"
+        {isEditing ? (
+          <Box
+            display={'flex'}
+            paddingTop={1}
+            paddingBottom={1}
+            marginRight={2}
           >
-            <ToggleButton value="prefere">Prefere</ToggleButton>
-            <ToggleButton value="dislike">Dislike</ToggleButton>
-          </ToggleButtonGroup>
-        </Box>
+            <ToggleButtonGroup
+              color="primary"
+              value={alignment}
+              exclusive
+              onChange={(event, value) => handleChange(event, value)}
+              aria-label="Platform"
+            >
+              <ToggleButton value="prefere">Prefere All</ToggleButton>
+              <ToggleButton value="dislike">Dislike All</ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+        ) : (
+          <Box
+            display={'flex'}
+            paddingTop={2}
+            // paddingBottom={1}
+            marginRight={3}
+          >
+            <Typography variant="h6">{mainPreference}</Typography>
+          </Box>
+        )}
       </Box>
       <Divider />
       <Collapse in={open} timeout="auto" unmountOnExit>
         <Box>
           <Table aria-label="shifts" sx={{ margin: '0' }}>
             <TableBody>
-              {shiftsIds.map((id) => {
+              {shiftPreferences
+                ? Object.keys(shiftPreferences).map((id) => {
+                    const { newPreference, hasChanged } = shiftPreferences[id]
+                    return (
+                      <TableRow key={id}>
+                        <TableCell
+                          component="th"
+                          scope="row"
+                          sx={{ textTransform: 'capitalize' }}
+                        >
+                          {shiftEntities[id]?.name}
+                        </TableCell>
+                        <TableCell align="right" sx={{ padding: 0, margin: 0 }}>
+                          <ToggleButtonGroup
+                            color="primary"
+                            size="small"
+                            value={newPreference}
+                            sx={{ marginRight: 2 }}
+                            exclusive
+                            onChange={(event, value) =>
+                              handleSingleChange(event, value, id)
+                            }
+                            aria-label="Platform"
+                          >
+                            <ToggleButton value="prefere">Prefere</ToggleButton>
+                            <ToggleButton value="dislike">Dislike</ToggleButton>
+                          </ToggleButtonGroup>
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })
+                : null}
+              {/* {shiftsIds.map((id) => {
+                const isValidShift = shiftEntities[id] ? true : false
                 const preferences = shiftEntities[id]?.preferences
                 let preferenceValue = null
                 if (
@@ -125,19 +279,27 @@ export default function PrefrencesItem(props: {
                   } else if (preferences.dislikedBy.includes(authUserId)) {
                     preferenceValue = 'disliked'
                   }
+                } else {
+                  console.log('Newtral Shift')
                 }
-                return (
+                return isValidShift ? (
                   <TableRow key={id}>
-                    <TableCell component="th" scope="row">
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      sx={{ textTransform: 'capitalize' }}
+                    >
                       {shiftEntities[id]?.name}
                     </TableCell>
-                    <TableCell align="right">
+                    <TableCell align="right" sx={{ padding: 0, margin: 0 }}>
                       <ToggleButtonGroup
                         color="primary"
+                        size="small"
                         value={preferenceValue}
+                        sx={{ marginRight: 2 }}
                         exclusive
                         onChange={(event, value) =>
-                          handleChange(event, value, id)
+                          handleSingleChange(event, value, id)
                         }
                         aria-label="Platform"
                       >
@@ -146,12 +308,36 @@ export default function PrefrencesItem(props: {
                       </ToggleButtonGroup>
                     </TableCell>
                   </TableRow>
-                )
-              })}
+                ) : null
+              })} */}
             </TableBody>
           </Table>
         </Box>
       </Collapse>
+      <Snackbar
+        open={openSuccessMsg}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          All Preferences updated!
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={openErrorMsg}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          Could not save preferences!
+        </Alert>
+      </Snackbar>
     </React.Fragment>
   )
+
+  const display = <React.Fragment></React.Fragment>
+
+  return content
 }
