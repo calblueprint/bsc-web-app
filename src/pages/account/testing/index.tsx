efe210ef29858cbd9b276c111a65daa7cafb0dcf
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2023-04-21 15:01:42 -0700

    added shift card info header and close button, testing on the testing page

--- src/pages/account/testing/index.tsx
+++ src/pages/account/testing/index.tsx
@@ -1,18 +1,33 @@
 // use this to test your stuff
 // NOTE: must be logged in as a manager to use this testing page
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { selectCurrentUser } from '@/features/auth/authSlice'
+import {
+  selectCurrentHouse,
+  selectCurrentUser,
+} from '@/features/auth/authSlice'
 import { Typography } from '@mui/material'
+import ShiftInfoHeader from '@/components/shared/shiftCardHeader/ShiftInfoHeader'
+import { useGetShiftsQuery } from '@/features/shift/shiftApiSlice'
 
 const TestingPage = () => {
-  const authUser = useSelector(selectCurrentUser)
-  console.log(authUser)
+  const { data } = useGetShiftsQuery('EUC')
+
+  // useEffect(() => {
+  //   console.log('Data Ids: ', data?.ids)
+  // }, [data])
 
   return (
     <React.Fragment>
       <Typography>Test test test</Typography>
+      {data ? (
+        <ShiftInfoHeader
+          shiftId={'1uCJCE9ePRnv4bMDEDRu'}
+          selectedDay="Friday"
+          handleClose={() => console.log('CLOOOOOSE')}
+        />
+      ) : null}
     </React.Fragment>
   )
 }
